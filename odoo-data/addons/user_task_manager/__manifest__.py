8479a57e31e9d608e117891f1fc3e45e3ecf3eee
Wilkins <wilkinfiguereo128@gmail.com>
2025-10-22 16:35:38 -0400

    Estructura proyecto v0.1

---
+++ odoo-data/addons/user_task_manager/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'User Task Manager',
+    'version': '1.0',
+    'author': 'Cybrosys Technologies',
+    'category': 'Productivity',
+    'website': 'https://www.cybrosys.com',
+    'summary': 'User Task Manager',
+    'description': 'Practica creacion de modulo. Gestor de tareas.',
+    'author': 'Wilkins',
+    'depends': ['base'],
+    'data': [
+        'security/task_security.xml',
+        'security/ir.model.access.csv',
+        'views/task_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
